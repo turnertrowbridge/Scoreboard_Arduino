46ecc27a0f78438330f18296efbbca5e0fc28c39
turnertrowbridge <ttrowbridge7322@sdsu.edu>
2023-12-05 14:30:42 -0800

    Added team colors

--- main/main.ino
+++ main/main.ino
@@ -16,6 +16,15 @@ const int rgbPins[] = {RGB_BLUE, RGB_GREEN, RGB_RED};
 #define A_BLUE A4
 const int basePins[] = {A_GREEN, A_YELLOW, A_BLUE};
 
+// Away team colors (Dodgers)
+#define AWAY_R 0
+#define AWAY_G 0
+#define AWAY_B 200
+
+// Home team colors (Padres)
+#define HOME_R 200
+#define HOME_G 200
+#define HOME_B 0
 
 // initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(8, 9, 10, 11, 12, 13);
@@ -31,240 +40,258 @@ int blueValue;
 
 
 void setup() {
-  // set up the LCD's number of columns and rows:
-  lcd.begin(16, 2);
-  Serial.begin(9600);
+    // set up the LCD's number of columns and rows:
+    lcd.begin(16, 2);
+    Serial.begin(9600);
 
-  // red lights
-  pinMode(RED1, OUTPUT);
-  pinMode(RED2, OUTPUT);
-  pinMode(RED3, OUTPUT);
+    // red lights
+    pinMode(RED1, OUTPUT);
+    pinMode(RED2, OUTPUT);
+    pinMode(RED3, OUTPUT);
 
-  // rgb pins
-  pinMode(RGB_RED, OUTPUT);
-  pinMode(RGB_GREEN, OUTPUT);
-  pinMode(RGB_BLUE, OUTPUT);
-  
-  redValue = 64;
-  greenValue = 224;
-  blueValue = 208;
+    // rgb pins
+    pinMode(RGB_RED, OUTPUT);
+    pinMode(RGB_GREEN, OUTPUT);
+    pinMode(RGB_BLUE, OUTPUT);
 
-  pinMode(A_GREEN, OUTPUT);
-  pinMode(A_YELLOW, OUTPUT);
-  pinMode(A_BLUE, OUTPUT);
+    redValue = 64;
+    greenValue = 224;
+    blueValue = 208;
+
+    pinMode(A_GREEN, OUTPUT);
+    pinMode(A_YELLOW, OUTPUT);
+    pinMode(A_BLUE, OUTPUT);
 }
 
 // triggers red led lights to display the outs
 void displayOuts(int outs) {
-  for (int i = 0; i < 3; i ++) {
-    if (i < outs) {
-      Serial.println(redPins[i]);
-      digitalWrite(redPins[i], HIGH);
-    } else {
-      digitalWrite(redPins[i], LOW);
+    for (int i = 0; i < 3; i++) {
+        if (i < outs) {
+            Serial.println(redPins[i]);
+            digitalWrite(redPins[i], HIGH);
+        } else {
+            digitalWrite(redPins[i], LOW);
+        }
     }
-  }
 }
 
 void displayBases(String bases) {
-  Serial.println("Displaying bases");
-  for (int i = 0; i < 3; i++) {
-    Serial.print(i);
-    Serial.println(bases.charAt(i));
-      if (bases.charAt(i) != '0') {
-        digitalWrite(basePins[i], HIGH);
-    } else {
-      digitalWrite(basePins[i], LOW);
+    Serial.println("Displaying bases");
+    for (int i = 0; i < 3; i++) {
+        Serial.print(i);
+        Serial.println(bases.charAt(i));
+        if (bases.charAt(i) != '0') {
+            digitalWrite(basePins[i], HIGH);
+        } else {
+            digitalWrite(basePins[i], LOW);
+        }
     }
-  }
 }
 
 void homeRun() {
-  int j = 2;
-  while (j > 0) {
-    // Red
-    analogWrite(RGB_RED, 255);
-    analogWrite(RGB_GREEN, 0);
-    analogWrite(RGB_BLUE, 0);
-    delay(100);
+    int j = 2;
+    while (j > 0) {
+        // Red
+        analogWrite(RGB_RED, 255);
+        analogWrite(RGB_GREEN, 0);
+        analogWrite(RGB_BLUE, 0);
+        delay(100);
 
-    // 1st base
-    digitalWrite(A_GREEN, HIGH);
-    digitalWrite(A_YELLOW, LOW);
-    digitalWrite(A_BLUE, LOW);
+        // 1st base
+        digitalWrite(A_GREEN, HIGH);
+        digitalWrite(A_YELLOW, LOW);
+        digitalWrite(A_BLUE, LOW);
 
-    // RBG Off
-    analogWrite(RGB_RED, 0);
-    analogWrite(RGB_GREEN, 0);
-    analogWrite(RGB_BLUE, 0);
-    delay(100);
-
-
-    // Green
-    analogWrite(RGB_RED, 0);
-    analogWrite(RGB_GREEN, 255);
-    analogWrite(RGB_BLUE, 0);
-    delay(100);
-
-    // 2nd base
-    digitalWrite(A_GREEN, LOW);
-    digitalWrite(A_YELLOW, HIGH);
-    digitalWrite(A_BLUE, LOW);
-
-    // RGB OFF
-    analogWrite(RGB_RED, 0);
-    analogWrite(RGB_GREEN, 0);
-    analogWrite(RGB_BLUE, 0);
-    delay(100);
-
-    
-    // Blue
-    analogWrite(RGB_RED, 0);
-    analogWrite(RGB_GREEN, 0);
-    analogWrite(RGB_BLUE, 255);
-    delay(100);
-
-    // 3rd base
-    digitalWrite(A_GREEN, LOW);
-    digitalWrite(A_YELLOW, LOW);
-    digitalWrite(A_BLUE, HIGH);
-
-    // RBG Off
-    analogWrite(RGB_RED, 0);
-    analogWrite(RGB_GREEN, 0);
-    analogWrite(RGB_BLUE, 0);
-    delay(100);
+        // RBG Off
+        analogWrite(RGB_RED, 0);
+        analogWrite(RGB_GREEN, 0);
+        analogWrite(RGB_BLUE, 0);
+        delay(100);
 
 
-    // White
-    analogWrite(RGB_RED, 255);
-    analogWrite(RGB_GREEN, 255);
-    analogWrite(RGB_BLUE, 255);
-    delay(100);
+        // Green
+        analogWrite(RGB_RED, 0);
+        analogWrite(RGB_GREEN, 255);
+        analogWrite(RGB_BLUE, 0);
+        delay(100);
 
-    // RBG off
-    analogWrite(RGB_RED, 0);
-    analogWrite(RGB_GREEN, 0);
-    analogWrite(RGB_BLUE, 0);
-    delay(100);
+        // 2nd base
+        digitalWrite(A_GREEN, LOW);
+        digitalWrite(A_YELLOW, HIGH);
+        digitalWrite(A_BLUE, LOW);
 
-    // Bases off
-    digitalWrite(A_GREEN, LOW);
-    digitalWrite(A_YELLOW, LOW);
-    digitalWrite(A_BLUE, LOW);
+        // RGB OFF
+        analogWrite(RGB_RED, 0);
+        analogWrite(RGB_GREEN, 0);
+        analogWrite(RGB_BLUE, 0);
+        delay(100);
 
-    j--;
+
+        // Blue
+        analogWrite(RGB_RED, 0);
+        analogWrite(RGB_GREEN, 0);
+        analogWrite(RGB_BLUE, 255);
+        delay(100);
+
+        // 3rd base
+        digitalWrite(A_GREEN, LOW);
+        digitalWrite(A_YELLOW, LOW);
+        digitalWrite(A_BLUE, HIGH);
+
+        // RBG Off
+        analogWrite(RGB_RED, 0);
+        analogWrite(RGB_GREEN, 0);
+        analogWrite(RGB_BLUE, 0);
+        delay(100);
+
+
+        // White
+        analogWrite(RGB_RED, 255);
+        analogWrite(RGB_GREEN, 255);
+        analogWrite(RGB_BLUE, 255);
+        delay(100);
+
+        // RBG off
+        analogWrite(RGB_RED, 0);
+        analogWrite(RGB_GREEN, 0);
+        analogWrite(RGB_BLUE, 0);
+        delay(100);
+
+        // Bases off
+        digitalWrite(A_GREEN, LOW);
+        digitalWrite(A_YELLOW, LOW);
+        digitalWrite(A_BLUE, LOW);
+
+        j--;
+    }
 }
+
+void setTeamColor(int r, int g, int b) {
+    analogWrite(RGB_RED, r);
+    analogWrite(RGB_GREEN, g);
+    analogWrite(RGB_BLUE, b);
 }
 
 void updateScreen() {
-  lcd.clear(); // Clear the display
-  lcd.setCursor(0, 0); // Set cursor to start of first row
-  lcd.print(currentText[0]); // Display text for the first row
-  lcd.setCursor(0, 1); // Set cursor to start of second row
-  lcd.print(currentText[1]); // Display text for the second row
+    lcd.clear(); // Clear the display
+    lcd.setCursor(0, 0); // Set cursor to start of first row
+    lcd.print(currentText[0]); // Display text for the first row
+    lcd.setCursor(0, 1); // Set cursor to start of second row
+    lcd.print(currentText[1]); // Display text for the second row
 }
 
 void loop() {
-  if (Serial.available()) {
-    Serial.println("Connected");
-    String jsonStr = Serial.readStringUntil('\n'); // Read the incoming JSON string
+    if (Serial.available()) {
+        Serial.println("Connected");
+        String jsonStr = Serial.readStringUntil('\n'); // Read the incoming JSON string
 
-    JSONVar game_data = JSON.parse(jsonStr); // Parse the received JSON string
+        JSONVar game_data = JSON.parse(jsonStr); // Parse the received JSON string
 
-    if (JSON.typeof(game_data) == "undefined") {
-      Serial.println("Failed to parse JSON");
-      // Serial.println(jsonStr); // Print the JSON string received
-  
-    } else {
+        if (JSON.typeof(game_data) == "undefined") {
+            Serial.println("Failed to parse JSON");
+            Serial.println(jsonStr); // Print the JSON string received
 
-        // Access JSON data
-        String awayTeam = game_data["aT"];
-        String awayAbrv = game_data["aA"];
-        String homeTeam = game_data["hT"];
-        String homeAbrv = game_data["hA"];
-        int awayScore = game_data["aS"];
-        int homeScore = game_data["hS"];
-        int strikes = game_data["cnt"][0];
-        int balls = game_data["cnt"][1];
-        int inning = game_data["in"];
-        String inningHalf = game_data["iH"];
-        String lastPlay = game_data["lP"];
-        int outs = game_data["outs"];
-        String bases = game_data["bases"];
-
-        Serial.print("Strikes: ");
-        Serial.println(bases);
-
-        String teamsFullDisplay = awayTeam + String(" v ") + homeTeam;
-        String teamAbrvDisplay = awayAbrv + String(" v ") + homeAbrv;
-        String score = awayScore + String("-") + homeScore;
-        String count = strikes + String("-") + balls;
-        char symbol = (inningHalf.equals("top")) ? 't' : 'b';
-
-        String inningStatus = symbol + String(inning);
-
-        currentText[0] = teamAbrvDisplay;
-
-
-        if (awayTeam == "TBD"){
-          currentText[0] = String("No Games");
-          currentText[1] = String("Right Now");
-          updateScreen();
-
-          // Outs
-          digitalWrite(RED1, LOW);
-          digitalWrite(RED2, LOW);
-          digitalWrite(RED3, LOW);
-
-          // Bases
-          digitalWrite(A_GREEN, LOW);
-          digitalWrite(A_YELLOW, LOW);
-          digitalWrite(A_BLUE, LOW);
-
-          // RGB
-          analogWrite(RGB_RED, 0);
-          analogWrite(RGB_GREEN, 0);
-          analogWrite(RGB_BLUE, 0);
-
-          savedLastPlay = "";
-
-        } else{
-
-        if (savedLastPlay != lastPlay) {
-          currentText[0] = teamsFullDisplay;
-          currentText[1] = lastPlay;
-          updateScreen();
-          savedLastPlay = lastPlay;
-
-          if (lastPlay == "Home Run") {
-            homeRun();
-          }
-
-          if (lastPlay == "Game End") {
-            currentText[0] = String("Game End");
-            currentText[1] = awayAbrv + String(" - ") + awayScore + String("  ") + homeAbrv + String(" - ") + homeScore;
-            savedLastPlay = "ge";
-            updateScreen();
-          }
         } else {
-          currentText[1] = String("S:") + score + String(" ") + String("C:") + count + String(" ") + inningStatus;
-          updateScreen();
+            Serial.println(jsonStr); // Print the JSON string received
+            // Access JSON data
+            String awayTeam = game_data["aT"];
+            String awayAbrv = game_data["aA"];
+            String homeTeam = game_data["hT"];
+            String homeAbrv = game_data["hA"];
+            int awayScore = game_data["aS"];
+            int homeScore = game_data["hS"];
+            int strikes = game_data["cnt"][0];
+            int balls = game_data["cnt"][1];
+            int inning = game_data["in"];
+            String inningHalf = game_data["iH"];
+            String lastPlay = game_data["lP"];
+            int outs = game_data["outs"];
+            String bases = game_data["bases"];
+
+            String teamsFullDisplay = awayTeam + String(" v ") + homeTeam;
+            String teamAbrvDisplay = awayAbrv + String(" v ") + homeAbrv;
+            String score = awayScore + String("-") + homeScore;
+            String count = strikes + String("-") + balls;
+            char symbol = (inningHalf.equals("top")) ? 't' : 'b';
+
+            String inningStatus = symbol + String(inning);
+
+            currentText[0] = teamAbrvDisplay;
+
+
+            if (awayTeam == "TBD") {
+                currentText[0] = String("No Games");
+                currentText[1] = String("Right Now");
+                updateScreen();
+
+                // Outs
+                digitalWrite(RED1, LOW);
+                digitalWrite(RED2, LOW);
+                digitalWrite(RED3, LOW);
+
+                // Bases
+                digitalWrite(A_GREEN, LOW);
+                digitalWrite(A_YELLOW, LOW);
+                digitalWrite(A_BLUE, LOW);
+
+                // RGB
+                analogWrite(RGB_RED, 0);
+                analogWrite(RGB_GREEN, 0);
+                analogWrite(RGB_BLUE, 0);
+
+                savedLastPlay = "";
+
+            } else {
+
+                if (savedLastPlay != lastPlay) {
+                    currentText[0] = teamsFullDisplay;
+                    currentText[1] = lastPlay;
+                    updateScreen();
+                    savedLastPlay = lastPlay;
+
+                    if (lastPlay == "Home Run") {
+                        homeRun();
+                    }
+
+                    if (lastPlay == "Game End") {
+                        currentText[0] = String("Game End");
+                        currentText[1] =
+                                awayAbrv + String(" - ") + awayScore + String("  ") + homeAbrv + String(" - ") +
+                                homeScore;
+                        savedLastPlay = "ge";
+                        updateScreen();
+                    }
+                } else {
+                    currentText[1] =
+                            String("S:") + score + String(" ") + String("C:") + count + String(" ") + inningStatus;
+                    updateScreen();
+                }
+
+
+                if (savedOuts != outs) {
+                    displayOuts(outs);
+                    savedOuts = outs;
+                }
+
+                if (savedBases != bases) {
+                    displayBases(bases);
+                    savedBases = bases;
+                }
+
+                if (inningHalf == "top" && savedLastPlay != "ge") {
+                    setTeamColor(AWAY_R, AWAY_G, AWAY_B);
+                } else if (savedLastPlay != "ge") {
+                    setTeamColor(HOME_R, HOME_G, HOME_B);
+                } else {
+                    analogWrite(RGB_RED, 0);
+                    analogWrite(RGB_GREEN, 0);
+                    analogWrite(RGB_BLUE, 0);
+                }
+
+                delay(1000); // delay 3 seconds
+            }
         }
-
-
-        if (savedOuts != outs) {
-          displayOuts(outs);
-          savedOuts = outs;
-        }
-
-        if (savedBases != bases) {
-          displayBases(bases);
-          savedBases = bases;
-        }   
-
-      delay(1000); // delay 3 seconds
     }
-  }
 }
+
 
